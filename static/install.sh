0863ba1cf70de3154622ad4577e0ce319515a011
joshuamckenty <jmckenty@gmail.com>
2023-05-20 18:10:36 +0000

    Build: (e56481e) Build scripts for golang

--- static/install.sh
+++ static/install.sh
@@ -2,5 +2,13 @@
 
 echo "Installing loophost..."
 
+if ! python3 -c 'import sys; assert sys.version_info >= (3,9)' > /dev/null; then
+    echo "You need a newer version of python installed first."
+    echo "We recommend following the instructions at https://docs.python-guide.org/starting/install3/osx/."
+    open "https://docs.python-guide.org/starting/install3/osx/"
+    exit 1
+fi
+
+
 pip install fling-hub
 python -m fling_hub.postinstall
