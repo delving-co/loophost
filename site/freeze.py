873d8d037aa6b22abacf1fa7fb837cd45d3ed9d7
Joshua McKenty <jmckenty@gmail.com>
2023-05-20 15:31:04 -0700

    Default mimetype

--- site/freeze.py
+++ site/freeze.py
@@ -2,6 +2,7 @@ from flask_frozen import Freezer
 from app import app
 
 freezer = Freezer(app)
+app.config['FREEZER_DEFAULT_MIMETYPE'] = 'text/html'
 
 if __name__ == '__main__':
     freezer.freeze()
