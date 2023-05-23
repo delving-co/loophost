a34ac579f306683b73e9ae5e80e3a4e0a8ae1d6b
Joshua McKenty <jmckenty@gmail.com>
2023-05-23 15:40:00 -0700

    Gotta yield all the pages.

--- site/freeze.py
+++ site/freeze.py
@@ -8,6 +8,10 @@ app.config["FREEZER_DEFAULT_MIMETYPE"] = "text/html"
 @freezer.register_generator
 def extra_url_generator():
     yield "/exit_survey.html"
+    yield "/tech.html"
+    yield "/update.html"
+    yield "/upgrade.html"
+    yield "/"
 
 
 if __name__ == "__main__":
